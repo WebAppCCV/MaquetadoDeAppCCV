9ab8de56e366f375e1f1aa9142e5084b49ee0ae5
DavidPadilla12 <davidpadilla12.93@gmail.com>
2016-04-06 00:19:54 -0500

    Listas con funcionalidad para consulta interna
    Consulta de indicador interno
    Tareas:
    Averiguar sobre la API de Gráficas.

--- prueba.js
+++ prueba.js
@@ -1,66 +1,50 @@
-$(function() {
-	var $ = window.jQuery
-	var apyKeyDiccionario = "5a53027e-43ed-4d60-9195-1dedde259b5e";
-	var apyKeyRecurso = "d3e76c49-06f3-4a66-9116-15fa4da62e25";
-	var urlConsulta = "http://datos.ciudatos.com/api/action/datastore_search?resource_id=";
-	var datosCali={};
-	datosCali.id,datosCali.indicador,datosCali.fuente;
+var $ = window.jQuery
+var apyKeyDiccionario = "5a53027e-43ed-4d60-9195-1dedde259b5e";
+var apyKeyRecurso = "d3e76c49-06f3-4a66-9116-15fa4da62e25";
+var urlConsulta = "http://datos.ciudatos.com/api/action/datastore_search?resource_id=";
+var vinculoIndicadores;
+var datosCali={};
+datosCali.id,datosCali.indicador,datosCali.tema;
 
+function indicador($this){
+	var id = $this.id;
+	console.log(id);
+	var consultaIndicador = urlConsulta+apyKeyRecurso+'&fields=ANIO,'+id;
+	$.getJSON(consultaIndicador, getDatosIndicador);
+}
+
+function getDatosIndicador(data){
+	var datos = data.result.records;
+	console.log(datos);
+}
+
+$(function() {
 	//var inputSearch = $("[data-input='busquedaIndicador']");
 	//var buttonSearch = $("[data-button='search']");
 	//$(button).on("click", resultadoBoton);
 
-	// function indicador($this){
-	    // var tema = $this.id;
-	    var consultaDiccionario = urlConsulta+apyKeyDiccionario+"&limit=1000";
-	   	//var consultaDiccionario = urlConsulta+apyKeyDiccionario+'&q={"tema":"'+tema+'"}'+'&limit=3';
-	    $.getJSON(consultaDiccionario, getIndicadores);
+	//var consultaDiccionario = urlConsulta+apyKeyDiccionario+'&q={"tema":"'+tema+'"}'+'&limit=3';
+	var consultaDiccionario = urlConsulta+apyKeyDiccionario+"&limit=1000";
+	$.getJSON(consultaDiccionario, getNombreIndicadores);
 
-
-	// }
-
-	function getIndicadores(data){
+	function getNombreIndicadores(data){
 		var datos = data.result.records;
-		console.log("Entree");
 		$.each( datos , function( i, item ) {
 			var dato = datos[i];
+			datosCali.tema = dato.tema;
 			datosCali.id = dato.id;
 			datosCali.indicador = dato.Indicador;
-			// renderTemplate();
-
-
+			vinculoIndicadores = "<a onclick='indicador(this)' id='"+datosCali.id+"'><i>"+datosCali.indicador+"</i></a>"
 
 			if(datosCali.tema=="1. Población y Demografía"){
-				$("[data-indicador='poblacion']").append('<a onclick="indicador(this)" id="'+datosCali.id+'"><i>'+datosCali.indicador+'</i></a>');
-	
+				$("[data-Indicador='poblacion']").append(vinculoIndicadores);
 			}
 			if(datosCali.tema=="3. Salud"){
-				$("[data-indicador='salud']").append('<a onclick="indicador(this)" id="'+datosCali.id+'"><i>'+datosCali.indicador+'</i></a>');
-	
+				$("[data-Indicador='salud']").append(vinculoIndicadores);
 			}
 			if(datosCali.tema=="4. Educación"){
-				$("[data-indicador='educacion']").append('<a onclick="indicador(this)" id="'+datosCali.id+'"><i>'+datosCali.indicador+'</i></a>');
-	
+				$("[data-Indicador='educacion']").append(vinculoIndicadores);
 			}
-			
 	   	 })
 	};
-
-	function indicador($this){
-		var id = $this.id;
-		alert("Accion: "+id);
-		//var consultaIndicador = urlConsulta+apyKeyRecurso+'&q={"id":"'+id+'"}'+'&limit=3';
-
-	}
-
-	// function activateTemplate(id){
-	// 	var t = document.querySelector(id);
-	// 	return  document.importNode(t.content, true);
-	// };
-
-	// function renderTemplate(){
-	// 	var clone = activateTemplate("#template-indicadores");
-	// 	clone.querySelector("[data-indicador]").innerHTML = datosCali.indicador;
-	// 	$("#nombreIndicador").append(clone);
-	// }
 });
